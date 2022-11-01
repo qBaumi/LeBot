5f3743524df91150ac0fc349a4560376060a0253
qBaumi <max05posch@gmail.com>
2022-11-01 23:57:33 +0100

    role assignment

--- cogs/basic.py
+++ cogs/basic.py
@@ -7,6 +7,41 @@ from discord import app_commands
 import dbutils
 from config import guilds
 
+ROLES = [
+    {"name": "League of Legends", "role_id": 1037133173919858829},
+    {"name": "Valorant", "role_id": 1037094545965461556},
+    {"name": "Genshin", "role_id": 1037133179150151731},
+]
+
+class RoleButton(discord.ui.Button):
+
+    def __init__(self, role):
+        self.role = role
+        # if the role has the "emoji" property it will be on the button aswell
+        try:
+            super().__init__(emoji=role["emoji"], style=discord.ButtonStyle.blurple, label=role["name"], custom_id=role["name"])
+        except:
+            super().__init__(style=discord.ButtonStyle.green, label=role["name"], custom_id=role["name"])
+    async def callback(self, interaction):
+        # Switch out the role
+        user_roles = interaction.user.roles
+        role = discord.utils.get(interaction.guild.roles, id=self.role["role_id"])
+        if role in user_roles:
+            await interaction.user.remove_roles(role)
+        else:
+            await interaction.user.add_roles(role)
+
+        try:
+            await interaction.response.send_message()
+        except:
+            return
+class RoleButtonView(discord.ui.View):
+    def __init__(self, roles):
+        super().__init__(timeout=None)
+        for role in roles:
+            button = RoleButton(role)
+            self.add_item(button)
+
 
 class basic(commands.Cog):
     def __init__(self, client):
@@ -16,6 +51,15 @@ class basic(commands.Cog):
     @app_commands.command(name="test", description="test")
     async def test(self, interaction: discord.Interaction):
         await interaction.response.send_message("Deez Nuts")
+
+    @commands.command()
+    async def roleassignment(self, ctx):
+        em = discord.Embed(title="Roles", description="Hello and welcome, here you can select your roles!",
+                           colour=discord.Color.dark_teal())
+        await ctx.send(embed=em)
+        em = discord.Embed(title="What games are you interested in?", colour=discord.Color.dark_teal())
+        await ctx.send(embed=em, view=RoleButtonView(ROLES))
+
 
 async def setup(client):
     await client.add_cog(basic(client), guilds=guilds)
--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import random
 import discord
 from discord.ext import commands
 import dbutils
+from cogs.basic import RoleButtonView, ROLES
 from config import token
 from discord import app_commands
 from config import guilds, guild
 
 client = commands.Bot(case_insensitive=True, command_prefix="joemama ", intents=discord.Intents.all())
 client.remove_command("help")
+
+ROLEASSIGNMENT_MESSAGE_ID = 1037133543886819428
 
 
 @client.event
@@ -23,6 +26,7 @@ async def on_ready():
 @client.event
 async def setup_hook():
     await client.load_extension("cogs.basic")
+    client.add_view(RoleButtonView(ROLES), message_id=ROLEASSIGNMENT_MESSAGE_ID)
 
 
 
